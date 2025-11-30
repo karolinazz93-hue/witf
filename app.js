73e7e6f4f1e00ed5485175259659e686e66c7817
karolinazz93-hue <karolinazz93@gmail.com>
2025-11-30 12:26:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,29 +2,16 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
 import { getDatabase, ref, set, onValue, remove, push } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCQz3cfhvrj1k6u-jWX8pvLBlBh-PvfUgo",
-  authDomain: "witf-app.firebaseapp.com",
-  databaseURL: "https://witf-app-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "witf-app",
-  storageBucket: "witf-app.firebasestorage.app",
-  messagingSenderId: "576239871878",
-  appId: "1:576239871878:web:9f60b6ed0b7f511f0b6e07",
-  measurementId: "G-SJTWDGYTSL"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-    appId: "1:123456789012:web:abcdefghijklmnop"
+    apiKey: "AIzaSyCQz3cfhvrj1k6u-jWX8pvLBlBh-PvfUgo",
+    authDomain: "witf-app.firebaseapp.com",
+    databaseURL: "https://witf-app-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "witf-app",
+    storageBucket: "witf-app.firebasestorage.app",
+    messagingSenderId: "576239871878",
+    appId: "1:576239871878:web:9f60b6ed0b7f511f0b6e07",
+    measurementId: "G-SJTWDGYTSL"
 };
 
 // Initialize Firebase
